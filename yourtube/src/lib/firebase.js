ee7f173826810e0b5b1f28992ec23f48a2abd383
rahul031102 <207742957+rahul031102@users.noreply.github.com>
2026-06-29 22:18:33 +0530

    feat: initialize firebase configuration and configure google auth provider

--- yourtube/src/lib/firebase.js
+++ yourtube/src/lib/firebase.js
@@ -20,8 +20,13 @@
 // const provider = new GoogleAuthProvider();
 // export { auth, provider };
 
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
